729744c740cd6dc37f1d4d9d94dcc756bcf2a6ae
greyvy <greyvy@gmail.com>
2013-06-02 13:07:16 -0700

    Added a description meta tag to the header

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -18,6 +18,8 @@
 		
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
 		
+		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	  	
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
